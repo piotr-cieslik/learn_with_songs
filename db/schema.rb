f772344b8c4fcda8928b3fe7b31b4dd5d951f13b
= <=>
2016-05-23 21:52:19 +0200

    changed migration and added first rspec test for user model

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,9 @@ ActiveRecord::Schema.define(version: 20160523194023) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
-    t.string   "password_hash"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    email ""
-    password_hash "MyString"
+    email "piotr.cieslik@outlook.com"
+    password "123456"
+    password_confirmation "123456"
   end
 end
