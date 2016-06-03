72e853dea8ec69563584209a0ec3dbb53bfbb77a
= <=>
2016-06-03 19:59:23 +0200

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all()
+Song.delete_all()
+
+user = User.create!(
+  email: "piotr.cieslik@outlook.com",
+  password: "123456",
+  password_confirmation: "123456"
+);
+
+(1..20).each() do |n|
+  song = user.songs.build(
+    title: "title #{n}",
+    author: "P. Cieślik",
+    lyrics: "uuuu, lalal, Ola!")
+  song.save!()
+end
