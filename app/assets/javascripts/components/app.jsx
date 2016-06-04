65db50b7f972e62df65a32694e22289748abadd4
= <=>
2016-06-04 15:12:40 +0200

    added notification bar and notification about successfull login / logout

--- app/assets/javascripts/components/app.jsx
+++ app/assets/javascripts/components/app.jsx
@@ -1,14 +1,37 @@
 var App = React.createClass({
   getInitialState: function() {
-    return { currentUser: null };
+    return {
+      currentUser: null,
+      notification: {
+        type: null,
+        message: null
+      }
+    };
   },
   handleUserSuccessfullyLogin: function(user){
-    this.setState({ currentUser: user });
+    this.setState({
+      currentUser: user,
+      notification: {
+        type: "success",
+        message: "Pomyślnie zalogowano."
+      }
+    });
   },
   handleUserSuccessfullyLogout: function(){
-    this.setState({ currentUser: null });
+    this.setState({
+      currentUser: null,
+      notification: {
+        type: "success",
+        message: "Pomyślnie wylogowano."
+      }
+    });
   },
   render: function() {
+    var notificationBar = (
+      <NotificationBar
+        type={ this.state.notification.type }
+        message={ this.state.notification.message } />
+    );
     if (this.state.currentUser) {
       var authorizationToken = this.state.currentUser['attributes']['auth-token'];
       return(
@@ -16,13 +39,19 @@ var App = React.createClass({
           <MenuBar
             authorizationToken={ authorizationToken }
             onUserSuccessfullyLogout ={ this.handleUserSuccessfullyLogout } />
+          { notificationBar }
           <SongsPage
             authorizationToken={ authorizationToken } />;
         </div>
       );
     }
-    return <LoginPage
-      onUserSuccessfullyLogin={ this.handleUserSuccessfullyLogin } />;
+    return(
+      <div>
+        { notificationBar }
+        <LoginPage
+          onUserSuccessfullyLogin={ this.handleUserSuccessfullyLogin } />;
+      </div>
+    );
   }
 });
 
