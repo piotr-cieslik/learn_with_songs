9c41463627f9cd990f1454241e8e2ca1dc2798f6
= <=>
2016-06-07 20:32:51 +0200

    remove notifiacation bar

--- app/assets/javascripts/components/app.jsx
+++ app/assets/javascripts/components/app.jsx
@@ -1,60 +1,30 @@
 var App = React.createClass({
   getInitialState: function() {
     var state = appStore.getState();
-    return {
-      url: state.url,
-      notification: {
-        type: null,
-        message: null
-      }
-    };
+    return{ url: state.url };
   },
   componentDidMount: function() {
     appStore.subscribe(this.handleStoreStateChange);
   },
   handleUserSuccessfullyLogin: function(user){
-    this.setState({
-      notification: {
-        type: "success",
-        message: "Pomyślnie zalogowano."
-      }
-    });
+    appStore.dispatch(Actions.goToPage('/songs'));
   },
   handleUserErroneouslyLogin: function(){
-    this.setState({
-      notification: {
-        type: "danger",
-        message: "Niepoprawny email lub hasło."
-      }
-    });
+    appStore.dispatch(Actions.goToPage('/login'));
   },
   handleUserSuccessfullyLogout: function(){
-    this.setState({
-      notification: {
-        type: "success",
-        message: "Pomyślnie wylogowano."
-      }
-    });
+    appStore.dispatch(Actions.goToPage('/login'));
   },
   handleStoreStateChange: function(){
     this.setState({ url: appStore.getState().url });
   },
   render: function() {
-    var notificationBar = (
-      <NotificationBar
-        type={ this.state.notification.type }
-        message={ this.state.notification.message } />
-    );
-
     var currentUser = CurrentUser.get();
     if (!currentUser) {
       return(
-        <div>
-          { notificationBar }
-          <LoginPage
-            onUserSuccessfullyLogin={ this.handleUserSuccessfullyLogin }
-            onUserErroneouslyLogin={this.handleUserErroneouslyLogin} />;
-        </div>
+        <LoginPage
+          onUserSuccessfullyLogin={ this.handleUserSuccessfullyLogin }
+          onUserErroneouslyLogin={this.handleUserErroneouslyLogin} />
       );
     }
 
