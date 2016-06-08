03e5fca61c41a4bee814940788882acd78a09056
= <=>
2016-06-08 16:38:56 +0200

    add different reducers for songs and user

--- app/assets/javascripts/components/protected_content/protected_content.jsx
+++ app/assets/javascripts/components/protected_content/protected_content.jsx
@@ -1,3 +1,3 @@
 var ProtectedContent = function(props){
-  return { props.children };
+  return null;
 };
--- app/assets/javascripts/redux_store.js
+++ app/assets/javascripts/redux_store.js
@@ -2,71 +2,76 @@ const FILL_SONGS = 'FILL_SONGS';
 const CREATE_SONG = 'CREATE_SONG';
 const DELETE_SONG = 'DELETE_SONG';
 const GO_TO_PAGE = 'GO_TO_PAGE';
+const LOGIN = 'LOGIN';
+const LOGOUT = 'LOGOUT';
 
 Actions = {
   fillSongs: function(songs){
-    return{
-      type: FILL_SONGS,
-      songs: songs
-    }
+    return{ type: FILL_SONGS, songs: songs }
   },
   createSong: function(song){
-    return{
-      type: CREATE_SONG,
-      song: song
-    }
+    return{ type: CREATE_SONG, song: song }
   },
   deleteSong: function(songId){
-    return{
-      type: DELETE_SONG,
-      songId: songId
-    }
+    return{type: DELETE_SONG, songId: songId }
   },
   goToPage: function(url){
-    return{
-      type: GO_TO_PAGE,
-      url: url
-    }
+    return{ type: GO_TO_PAGE, url: url }
+  },
+  login: function(user){
+    return { type: LOGIN, user: user }
+  },
+  logout: function(){
+    return { type: LOGOUT }
   }
 };
 
-var appReducer = function(state, action) {
+var songsReducer = function(state, action) {
   if (typeof state === 'undefined') {
-    return{ songs:[], url: null }
+    return [];
   }
 
   if(action.type === FILL_SONGS){
-    state.songs = action.songs;
-
-    return state;
+    return action.songs;
   }
 
   if(action.type === CREATE_SONG){
-    state.songs.push(action.song);
-    state.songs = state.songs.slice();
-
-    return state;
+    state.push(action.song);
+    return state.songs.slice();
   }
 
   if(action.type === DELETE_SONG){
-    var songToDelete = state.songs.filter(function(s){
+    var songToDelete = state.filter(function(s){
       return s.id == action.songId;
     })[0];
-    var index = state.songs.indexOf(songToDelete);
-    state.songs.splice(index, 1);
-    var newSongs = state.songs.slice();
-    state.songs = newSongs;
+    var index = state.indexOf(songToDelete);
+    state.splice(index, 1);
 
-    return state;
+    return state.slice();
   }
 
-  if(action.type === GO_TO_PAGE){
-    state.url = action.url;
-
-    return state;
-  }
-
-  return state
+  return state;
 };
 
-var appStore = Redux.createStore(appReducer);
+var userReducer = function(state, action){
+  if (typeof state === 'undefined') {
+    return null;
+  }
+
+  if(action.type === LOGIN){
+    return action.user;
+  }
+
+  if(action.type === LOGOUT){
+    return null;
+  }
+
+  return state;
+}
+
+var reducers = Redux.combineReducers({
+  songs: songsReducer,
+  user: userReducer
+});
+
+var appStore = Redux.createStore(reducers);
