30c8d6f6b673555080e62121d82f88280972bf6d
= <=>
2016-06-09 20:52:21 +0200

    simplify song details

--- app/assets/javascripts/components/protected_content/songs/song_details.jsx
+++ app/assets/javascripts/components/protected_content/songs/song_details.jsx
@@ -8,20 +8,15 @@ var SongDetails = React.createClass({
     }
 
     return(
-      <div className="panel panel-default">
-        <div className="panel-heading">
-          { this.props.song.attributes.author } - { this.props.song.attributes.title }
-        </div>
-        <div className="panel-body">
-          <pre>
-            { this.props.song.attributes.lyrics }
-          </pre>
-          <div className="btn-toolbar pull-right">
-            <button
-              type="button"
-              className="btn btn-danger"
-              onClick={ this.handleDeleteSong }>Usuń</button>
-          </div>
+      <div>
+        <pre>
+          { this.props.song.attributes.lyrics }
+        </pre>
+        <div className="btn-toolbar pull-right">
+          <button
+            type="button"
+            className="btn btn-danger"
+            onClick={ this.handleDeleteSong }>Usuń</button>
         </div>
       </div>
     );
