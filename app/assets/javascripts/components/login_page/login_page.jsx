574afdae39d55349dacee3c93f24e34e99837864
= <=>
2016-06-10 19:19:39 +0200

    put loggin form into the card

--- app/assets/javascripts/components/login_page/login_page.jsx
+++ app/assets/javascripts/components/login_page/login_page.jsx
@@ -1,33 +1,41 @@
 var LoginPage = function(params){
   return(
     <div className="container">
-      <form
-        onSubmit={ params.onSubmit } >
-        <div
-          className="input-field">
-          <input
-            id="email"
-            type="text"
-            value={ params.email }
-            onChange={ params.onEmailChange } />
-          <label for="email">Email:</label>
+      <div className="row">
+        <div className="col l8">
+          <div className="card">
+            <div className="card-content">
+              <form
+                onSubmit={ params.onSubmit } >
+                <div
+                  className="input-field">
+                  <input
+                    id="email"
+                    type="text"
+                    value={ params.email }
+                    onChange={ params.onEmailChange } />
+                  <label for="email">Email:</label>
+                </div>
+                <div
+                  className="input-field">
+                  <input
+                    id="password"
+                    type="password"
+                    placeholder="hasło"
+                    value={ params.password }
+                    onChange={ params.onPasswordChange } />
+                  <label for="password">Hasło:</label>
+                </div>
+                <button
+                  className="btn waves-effect waves-light"
+                  type="submit">
+                  Zaloguj
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
-        <div
-          className="input-field">
-          <input
-            id="password"
-            type="password"
-            placeholder="hasło"
-            value={ params.password }
-            onChange={ params.onPasswordChange } />
-          <label for="password">Hasło:</label>
-        </div>
-        <button
-          className="btn waves-effect waves-light"
-          type="submit">
-          Zaloguj
-        </button>
-      </form>
+      </div>
     </div>
   )
 };
