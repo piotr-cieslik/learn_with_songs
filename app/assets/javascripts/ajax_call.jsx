18f0cbc33c5d472e1726efb35bf848fadd003e88
= <=>
2016-06-10 19:41:55 +0200

    changed BI

--- app/assets/javascripts/ajax_call.jsx
+++ app/assets/javascripts/ajax_call.jsx
@@ -1,6 +1,19 @@
+var busyIndicator =
+'<div class="preloader-wrapper active">'+
+  '<div class="spinner-layer spinner-red-only">' +
+    '<div class="circle-clipper left">' +
+      '<div class="circle"></div>' +
+    '</div><div class="gap-patch">' +
+      '<div class="circle"></div>' +
+    '</div><div class="circle-clipper right">' +
+      '<div class="circle"></div>'+
+    '</div>' +
+  '</div>' +
+'</div>';
+
 $(document).ajaxStart(function () {
   $.blockUI({
-    message: "<img src='assets/indicator.gif'></img>",
+    message: busyIndicator,
     css: {
       padding:        0,
       margin:         0,
