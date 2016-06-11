619efc8be5cf9fa19f4cc524da1e3c851a301a52
= <=>
2016-06-11 09:17:15 +0200

    protected content is not render when user is not logged in

--- app/assets/javascripts/components/protected_content/protected_content.jsx
+++ app/assets/javascripts/components/protected_content/protected_content.jsx
@@ -1,9 +1,15 @@
 var ProtectedContent = React.createClass({
-    render: function() {
-      return (
-        <Layout>
-          { this.props.children }
-        </Layout>
-      );
+  render: function() {
+    user = applicationStore.getState().user;
+    if(!user){
+      ReactRouter.browserHistory.push('/login');
+      return null;
     }
+
+    return (
+      <Layout>
+        { this.props.children }
+      </Layout>
+    );
+  }
 });
