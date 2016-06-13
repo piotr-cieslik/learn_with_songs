97d06a23b07b45440d8027d9960c56806f73bb37
= <=>
2016-06-13 21:32:03 +0200

    improve styles on login page

--- app/assets/javascripts/components/login_page/login_page.jsx
+++ app/assets/javascripts/components/login_page/login_page.jsx
@@ -1,37 +1,45 @@
 var LoginPage = function(params){
   return(
-    <div className="container">
-      <div className="row">
-        <div className="col l8">
-          <div className="card">
-            <div className="card-content">
-              <span className="card-title">Logowanie</span>
-              <form
-                onSubmit={ params.onSubmit } >
-                <div
-                  className="input-field">
-                  <label htmlFor="email">email</label>
-                  <input
-                    id="email"
-                    type="text"
-                    value={ params.email }
-                    onChange={ params.onEmailChange } />
-                </div>
-                <div
-                  className="input-field">
-                  <label htmlFor="password">hasło</label>
-                  <input
-                    id="password"
-                    type="password"
-                    value={ params.password }
-                    onChange={ params.onPasswordChange } />
-                </div>
-                <button
-                  className="btn waves-effect waves-light"
-                  type="submit">
-                  Zaloguj
-                </button>
-              </form>
+    <div id="main-page-banner">
+      <div className="container">
+        <div className="row">
+
+          <div className="col m6 s12">
+            <h1>Strona bez nazwy!</h1>
+            <p>Chwilowo, z podowu braku weny autora, strona nie posiada nazwy.</p>
+          </div>
+
+          <div className="col m6 s12">
+            <div className="card">
+              <div className="card-content">
+                <span className="card-title">Logowanie</span>
+                <form
+                  onSubmit={ params.onSubmit } >
+                  <div
+                    className="input-field">
+                    <label htmlFor="email">email</label>
+                    <input
+                      id="email"
+                      type="text"
+                      value={ params.email }
+                      onChange={ params.onEmailChange } />
+                  </div>
+                  <div
+                    className="input-field">
+                    <label htmlFor="password">hasło</label>
+                    <input
+                      id="password"
+                      type="password"
+                      value={ params.password }
+                      onChange={ params.onPasswordChange } />
+                  </div>
+                  <button
+                    className="btn waves-effect waves-light"
+                    type="submit">
+                    Zaloguj
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
         </div>
